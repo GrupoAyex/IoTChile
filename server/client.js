563f0d4824630a1c4e88575f549639bc2a41c945
Alejandro Bether <ahbehter@hotmail.com>
2019-01-25 08:59:13 -0300

    20190125

--- server/client.js
+++ server/client.js
@@ -1,11 +1,14 @@
 const WebSocket = require("ws");
-
-var ws = new WebSocket("https://sierra-io.herokuapp.com/");
-//const ws = new WebSocket("ws:localhost:3000");
+const moment = require("moment");
+var ws = new WebSocket("https:sierra-io.herokuapp.com");
+//const ws = new WebSocket("ws:10.10.3.2:3000");
+const _ = require("lodash");
 let wsOpen = false;
 
 ws.on("open", function open() {
   wsOpen = true;
+
+  console.log(ws.url);
   console.log("Sending Message to Server");
 });
 
@@ -21,11 +24,27 @@ setInterval(() => {
   if (wsOpen) {
     console.log("Sending new message");
     let message = {
-      TYPE: "meter",
-      UID: "macaddress",
-      COUTDENO: "111",
-      BILLDENO: "2222",
-      TIME: Date()
+      UID: 200821,
+      COUTDENO: _.ceil(Math.random() * 100),
+      BILLDENO: _.ceil(Math.random() * 1000),
+      PAYBACK: _.ceil(Math.random() * 10),
+      IPID: "10.10.3.4",
+      MACID: "84:0D:8E:84:25:7F",
+      ACHOUR: moment().format("H"),
+      ACMINUTE: 34,
+      ACSECOND: 7,
+      ACYEAR: 2019,
+      ACMONTH: 1,
+      ACDAY: 18,
+      ACDAYOFWK: 6,
+      COUT_PAY: 26,
+      ACCOINOUT: 806,
+      BILL_INC: 11,
+      ACBILLIN: 342,
+      TYPE: "METER",
+      ROLLOVER_C_B: [0, 0],
+      CRC: "31123",
+      TIME: moment().format("x")
     };
     ws.send(JSON.stringify(message));
   }
--- server/server.js
+++ server/server.js
@@ -3,21 +3,27 @@
  *
  *
  *********************************************************/
-
 require("../config/config");
 require("../startup/db")();
+const { saveData } = require("../config/firebase");
 
 const { Meter, validate } = require("../models/meters");
-const { ackMsg, noAckMsg, invalidTypeAckMsg } = require("../config/messages");
-
+const {
+  ackMsg,
+  noAckMsg,
+  invalidTypeAckMsg,
+  noJsonAckMsg
+} = require("../config/messages");
 const fs = require("fs");
-
 const http = require("http");
+const _ = require("lodash");
 
+//Generamos un servidor HTTP
 const httpServer = http.createServer();
-
 httpServer.listen(process.env.PORT, "0.0.0.0");
 const webSocketServer = require("ws").Server;
+//npm i ws --save
+//npm i ws@1.0.1
 
 const wss = new webSocketServer({
   server: httpServer,
@@ -36,44 +42,85 @@ wss.on("connection", (ws, req) => {
     Socket: ws
   };
 
+  /*
+  console.log({
+    client: clientInfo.ip,
+    message: JSON.stringify(packetReceived)
+  });
+  */
+
   //OnMessage Received
   ws.on("message", async function incoming(message) {
-    const packetReceived = JSON.parse(message);
-    console.log("Message Received : ", {
+    let packetReceived = {};
+
+    /*
+    try {
+      packetReceived = JSON.parse(message);
+    } catch {
+      console.log("ERROR", message);
+      return;
+    }
+    console.log({
       client: clientInfo.ip,
       message: JSON.stringify(packetReceived)
     });
+    */
+    try {
+      packetReceived = JSON.parse(message);
+
+      const { TYPE } = packetReceived;
+
+      switch (String(TYPE).toUpperCase()) {
+        case "METER":
+          //const { error } = validate(packetReceived);
+
+          const error = false;
+          if (error) {
+            //TODO ADD Validations
+            // console.log("DETALLE", error.details[0]);
+            ws.send(JSON.stringify(invalidTypeAckMsg));
+          } else {
+            /*
+            let meter = _.omit(new Meter(packetReceived), [
+              ACSECOND,
+              ACDAY,
+              TYPE,
+              ACMINUTE
+            ]);
+            */
+
+            //Parsear package
+
+            //saveData(_.omit(JSON.stringify(packetReceived), "ROLLOVER_C_B"));
+            saveData(packetReceived);
+
+            ws.send(JSON.stringify(ackMsg));
+
+            /* MONGO DB
+            await meter.save((err, data) => {
+              if (err) {
+                ws.send(JSON.stringify(noAckMsg));
+              } else {
+                ws.send(JSON.stringify(ackMsg));
+              }
+            });
+            */
+          }
+          break;
+        case "LOG":
+          break;
+        case "NOTDEFINED":
+          break;
+        default:
+          ws.send(JSON.stringify(invalidTypeAckMsg));
+      }
+    } catch (e) {
+      ws.send(JSON.stringify(noJsonAckMsg));
+      console.log("ERROR JSON MAL FORMED FROM -> " + clientInfo.ip, message);
+      return;
+    }
 
     //Get Packet Type
-    const { TYPE } = packetReceived;
-
-    switch (String(TYPE).toUpperCase()) {
-      case "METER":
-        const { error } = validate(packetReceived);
-
-        if (error) {
-          //TODO ADD Validations
-          // console.log("DETALLE", error.details[0]);
-          ws.send(JSON.stringify(invalidTypeAckMsg));
-        } else {
-          let meter = new Meter(packetReceived);
-
-          await meter.save((err, data) => {
-            if (err) {
-              ws.send(JSON.stringify(noAckMsg));
-            } else {
-              ws.send(JSON.stringify(ackMsg));
-            }
-          });
-        }
-        break;
-      case "LOG":
-        break;
-      case "NOTDEFINED":
-        break;
-      default:
-        ws.send(JSON.stringify(invalidTypeAckMsg));
-    }
   });
 
   /*
